64d08c9c23a1c5b4f8c775092bd0265b30dfe26e
Sayyidusy syauqi Al Ghiffari <88390553+Sayyidusy@users.noreply.github.com>
2024-08-13 20:25:34 +0700

    Loading for Client Side

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -3,19 +3,28 @@ import supabase from '../../utils/supabase'
 
 export default function About() {
     const [data, setData] = useState<any>(null);
+    const [loading, setloading] = useState<boolean>(true);
 
     useEffect(() => {
         const getData = async () => {
          const {data, error} = await supabase.from("personal").select();
          console.log({data, error});
-         
+         setloading(false);
          setData (data);
         };
 
         getData();
     },[]);
 
-    return (
+    return loading ? (
+        <p>Loading ...</p>
+    ) : (
         <pre>{JSON.stringify(data, null, 2)}</pre>
     )
 }
+
+
+// client-side => useEffect
+// server-rendered => getServerSideProps
+// static-generation => getStaticProps
+// static-with-revelidation => getStaticProps with revalidate key
